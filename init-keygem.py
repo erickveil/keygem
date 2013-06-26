a2047ca12d090d23f9fe1286bdacaad00d5b121b
Debian Live user <amnesia@amnesia.(none)>
2013-06-26 17:53:23 +0000

    now examining wether or not the f keys are defined in autokey
    Note: exiting early from the script while the keys are present in autokey
    results in failing to test the remaining conditions.

--- init-keygem.py
+++ init-keygem.py
@@ -5,7 +5,10 @@ Erick Veil
 2013-06-24
 
 """
-import json, sys, os
+import json, sys, os, getpass, subprocess
+
+folder_count=0
+keyset_flag=False
 
 """1.0.0"""
 def import_autokey_scripts():
@@ -15,9 +18,11 @@ def import_autokey_scripts():
         returns True on success.
 
     """
-    if get_is_fkeys_used():
+    used=get_is_fkeys_used()
+    if used!=-1 and used:
         return False
 
+    get_is_autokey_running()
     if get_is_autokey_running():
         suspend_autokey()
 
@@ -31,32 +36,79 @@ def import_autokey_scripts():
 def get_is_fkeys_used():
     """Determines if ctrl-F1/2/3 keys are defined in AutoKey
 
-    returns True if present, false if not.
+    returns True if present, false if not, -1 on error
     We don't want to define other keys that might cause conflict.
 
     """
+    global keyset_flag 
+
     user=get_linux_user_name()
     config_path="/home/"+user+"/.config/autokey/autokey.json"
-    
-    config_file=open(config_path,'r')
+
+    try:
+        config_file=open(config_path,'r')
+    except IOError:
+        return -1
 
     config_obj=json.load(config_file)
-    print config_obj
+
+    search_folders_for_hotkeys(config_obj)
 
     config_file.close()
 
-
-    return True
+    return keyset_flag
 
 """0.1.1"""
 def get_linux_user_name():
-    """placeholder"""
+    """Simply returns the linux username, pased on the environment variable."""
 
-    return "amnesia"
+    return getpass.getuser()
+
+"""0.1.2"""
+def search_folders_for_hotkeys(dic_root):
+    """Iterates through the autokey.json object and looks into each "folder"
+    member. Looks for "items" keys, and calls a function that looks through the
+    keys' members.
+    Uses recursion to hit nested folders.
+    """
+    global folder_count
+
+    for key in dic_root:
+        if key=="folders":
+            size=len(dic_root[key])
+            if size>0:
+                for i in range(0,size):
+                    search_folders_for_hotkeys(dic_root[key][i])
+        elif key=="items":
+            search_folder_items(dic_root[key])
+
+"""0.1.2.1"""
+def search_folder_items(items_root):
+    """the keyset_flag default is false.
+    Seaches through a given item and looks at any hotkey components. If found,
+    the hotkeys are checked for keys specific to keygem: f1-3, modified by
+    ctrl. If found, sets the keyset_flag to true, indicating that we should not
+    go inserting code that uses those hotkeys.
+    """
+    global keyset_flag
+
+    size=len(items_root)
+    for i in range(0,size):
+        autokey=items_root[i]["hotkey"]["hotKey"]
+        mods=items_root[i]["hotkey"]["modifiers"]
+        if len(mods)>0 and mods[0]=="<ctrl>":
+            if autokey=="<f1>" or autokey=="<f2>" or autokey=="<f3>":
+                keyset_flag=True
 
 """0.2.0"""
 def get_is_autokey_running():
     """placeholder"""
+
+    print "hello"
+    """result=subprocess.Popen("ps",stdout=subprocess.PIPE)
+    for line in result.stdout.readlines():
+        print ":"+line
+        """
 
     return True
 
